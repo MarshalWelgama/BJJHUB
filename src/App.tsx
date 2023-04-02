6b2a8249368872e5d91aeaae7e08dfcfad933906
Marshal Welgama <marshalwelgama@gmail.com>
2023-04-02 22:54:29 +1000

    new: styles, now playing and drop down button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import * as React from "react";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import ProTip from "./ProTip";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { MainNavigation } from "./screens/MainNavigation";
-import { AppBar, IconButton, Toolbar, Button } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { OpenInBrowserSharp } from "@mui/icons-material";
-import "./styles.css";
 
 export default function App() {
   return (
